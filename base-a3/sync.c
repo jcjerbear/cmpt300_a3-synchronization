f1643e3bbd37670a9837cd1a57b0585ae8a4cea2
jcjerbear <cyc42@sfu.ca>
2016-11-02 21:40:09 -0700

    first commit

---
+++ base-a3/sync.c
@@ -0,0 +1,90 @@
+/*
+ * NAME, etc.
+ *
+ * sync.c
+ *
+ */
+
+#define _REENTRANT
+
+#include "sync.h"
+
+
+/*
+ * Spinlock routines
+ */
+
+int my_spinlock_init(my_spinlock_t *lock)
+{
+}
+
+int my_spinlock_destroy(my_spinlock_t *lock)
+{
+}
+
+int my_spinlock_unlock(my_spinlock_t *lock)
+{
+}
+
+int my_spinlock_lockTAS(my_spinlock_t *lock)
+{
+}
+
+
+int my_spinlock_lockTTAS(my_spinlock_t *lock)
+{
+}
+
+int my_spinlock_trylock(my_spinlock_t *lock)
+{
+}
+
+
+/*
+ * Mutex routines
+ */
+
+int my_mutex_init(my_mutex_t *lock)
+{
+}
+
+int my_mutex_destroy(my_mutex_t *lock)
+{
+}
+
+int my_mutex_unlock(my_mutex_t *lock)
+{
+}
+
+int my_mutex_lock(my_mutex_t *lock)
+{
+}
+
+int my_mutex_trylock(my_mutex_t *lock)
+{
+}
+
+/*
+ * Queue Lock
+ */
+
+int my_queuelock_init(my_queuelock_t *lock)
+{
+}
+
+int my_queuelock_destroy(my_queuelock_t *lock)
+{
+}
+
+int my_queuelock_unlock(my_queuelock_t *lock)
+{
+}
+
+int my_queuelock_lock(my_queuelock_t *lock)
+{
+}
+
+int my_queuelock_trylock(my_queuelock_t *lock)
+{
+}
+
---
+++ base-a3/sync.h
@@ -0,0 +1,64 @@
+/*
+ * NAME, etc.
+ *
+ * sync.h
+ */
+
+#ifndef _m_SYNC_H_
+#define _m_SYNC_H_
+
+#include "atomic_ops.h"
+
+struct my_mutex_struct {
+  /* FILL ME IN! */
+};
+
+
+
+
+typedef struct my_mutex_struct my_mutex_t;
+
+
+int my_mutex_init(my_mutex_t *lock);
+int my_mutex_unlock(my_mutex_t *lock);
+int my_mutex_destroy(my_mutex_t *lock);
+
+int my_mutex_lock(my_mutex_t *lock);
+int my_mutex_trylock(my_mutex_t *lock);
+
+
+
+/*Spinlock Starts here*/
+
+struct my_spinlock_struct {
+  /* FILL ME IN! */
+};
+
+typedef struct my_spinlock_struct my_spinlock_t;
+
+int my_spinlock_init(my_spinlock_t *lock);
+int my_spinlock_destroy(my_spinlock_t *lock);
+int my_spinlock_unlock(my_spinlock_t *lock);
+
+int my_spinlock_lockTAS(my_spinlock_t *lock);
+int my_spinlock_lockTTAS(my_spinlock_t *lock);
+int my_spinlock_trylock(my_spinlock_t *lock);
+
+
+/*Spinlock Starts here*/
+
+struct my_queuelock_struct {
+  /* FILL ME IN! */
+};
+
+typedef struct my_queuelock_struct my_queuelock_t;
+
+int my_queuelock_init(my_queuelock_t *lock);
+int my_queuelock_destroy(my_queuelock_t *lock);
+int my_queuelock_unlock(my_queuelock_t *lock);
+
+int my_queuelock_lock(my_queuelock_t *lock);
+int my_queuelock_trylock(my_queuelock_t *lock);
+
+
+#endif
